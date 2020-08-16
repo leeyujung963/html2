dc9ce1805b1e95a7e594c779fc391447d35cecee
leeyujung963 <68464392+leeyujung963@users.noreply.github.com>
2020-08-16 18:49:40 +0900

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,6 @@ from django.views.generic import ListView, DetailView
 
 class PostList(ListView):
     model = Post
-    paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostList, self).get_context_data(**kwargs)
